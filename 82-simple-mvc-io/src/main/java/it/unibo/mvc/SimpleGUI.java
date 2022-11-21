ce52889321bf3430db87bd10bfe26a79d8e4ba16
Pietro Olivi <pietro.olivi2@studio.unibo.it>
2022-11-21 15:41:21 +0100

    Implemented 1st and 2nd part

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,15 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.WindowConstants;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,5 +18,32 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+    private static final int PROPORTION = 5;
 
+    public SimpleGUI() {
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        final JTextArea area = new JTextArea("Write text here");
+        panel.add(area, BorderLayout.CENTER);
+        final JButton save = new JButton("Save");
+        panel.add(save, BorderLayout.SOUTH);
+        frame.add(panel);
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                final Controller controller = new Controller();
+                controller.writeStringOnFile(area.getText());
+            }
+        });
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int width = (int) screen.getWidth();
+        final int height = (int) screen.getHeight();
+        frame.setSize(width / PROPORTION, height / PROPORTION);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SimpleGUI gui = new SimpleGUI();
+    }
 }
