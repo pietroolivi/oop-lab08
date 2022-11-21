9e79a7a35b7f5096f49ea6e675470bfa0c7eb9ba
Pietro Olivi <pietro.olivi2@studio.unibo.it>
2022-11-21 21:38:43 +0100

    Implemented Controller's methods

--- 83-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,48 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    private String currentString;
+    private final List<String> history;
+
+    /**
+    * Initialize the variables.
+    */
+    public SimpleController() {
+        this.currentString = null;
+        this.history = new ArrayList<>();
+    }
+
+    @Override
+    public void setNextStringToPrint(final String s) {
+        this.currentString = s;
+        this.history.add(this.currentString);
+    }
+
+    @Override
+    public String getNextStringToPrint() {
+        return this.currentString;
+    }
+
+    @Override
+    public List<String> getStringsHistory() {
+        return new ArrayList<String>(history);
+    }
+
+    @Override
+    public void printCurrentString() {
+        if (this.currentString == null) {
+            throw new IllegalStateException();
+        } else {
+            System.out.println(this.currentString); // NOPMD: allowed as this is just an exercise
+        }
+    }
 
 }
