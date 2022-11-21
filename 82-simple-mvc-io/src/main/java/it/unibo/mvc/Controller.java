6068ba0f070f72c8b66b477919fbb5163b01dc12
Pietro Olivi <pietro.olivi2@studio.unibo.it>
2022-11-21 14:46:35 +0100

    1st version

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,41 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Objects;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private static final String PATH = System.getProperty("user.home") + System.getProperty("file.separator");
+    private File currentFile = new File(PATH + "output.txt");
 
+    public void setCurrentFile(final File f) {
+        Objects.requireNonNull(f);
+        this.currentFile = f;
+    }
+
+    public void writeStringOnFile(final String message)  {
+        try (PrintWriter fileStream = new PrintWriter(currentFile)) {
+            fileStream.print(message);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public File getCurrentFile() {
+        return this.currentFile;
+    }
+
+    public String getCurrentFileName() {
+        return this.currentFile.getName();
+    }
+
+    public String getCurrentFilePath() {
+        return this.currentFile.getAbsolutePath();
+    }
 }
